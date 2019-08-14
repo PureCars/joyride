54a764adbaafa447e5f82c2e506905d70ba0d05c
Jim Noble <jimn@purecars.com>
2019-08-14 16:26:49 -0400

    Update styles.js

--- src/components/VehicleCard/styles.js
+++ src/components/VehicleCard/styles.js
@@ -1,19 +1,24 @@
-import styled from '@emotion/styled/macro'
+import styled from 'styled-components'
+import Typography from '@material-ui/core/Typography'
 
 export const VehicleImage = styled.img`
   overflow: hidden;
   width: 100%;
 `
 
-export const Title = styled.div`
-  font-size: 16px;
+export const Title = styled(Typography)`
+  padding-left: ${({ theme }) => theme.mui.spacing(1)}px;
+  padding-right: ${({ theme }) => theme.mui.spacing(1)}px;
 `
 
-export const Details = styled.div`
-  font-size: 14px;
+export const Details = styled(Typography)`
+padding-left: ${({ theme }) => theme.mui.spacing(1)}px;
+padding-right: ${({ theme }) => theme.mui.spacing(1)}px;
 `
 
-export const PriceLine = styled.div`
+export const PriceLine = styled(Typography)`
+  padding-left: ${({ theme }) => theme.mui.spacing(1)}px;
+  padding-right: ${({ theme }) => theme.mui.spacing(1)}px;
   display: flex;
   justify-content: space-between;
-`  
+` 
