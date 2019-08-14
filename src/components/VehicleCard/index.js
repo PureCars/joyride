84c625cf57a8602dc4e670017f59b12ce946c7af
Jordan Carroll <jordanc@purecars.com>
2019-08-14 16:57:15 -0400

    small changes to component structure

--- src/components/VehicleCard/index.js
+++ src/components/VehicleCard/index.js
@@ -1,17 +1,26 @@
 import React from 'react'
-import Card from '@material-ui/core/Card';
-import { VehicleImage, Title, Details, PriceLine } from './styles'
+import Card from '@material-ui/core/Card'
+import { VehicleImage, VehicleInfo, Title, Details, PriceLine } from './styles'
 
-const VehicleCard = ({ className, imageUrl, title, details, price, payment }) => (
-    <Card className={className}>
-        <VehicleImage alt="Test" src={ imageUrl } />
-        <Title>{ title }</Title>
-        <Details>{ details }</Details>
-        <PriceLine>
-            <div>{ price }</div>
-            <div>{ payment }</div>
-        </PriceLine>
-    </Card>
+const VehicleCard = ({
+  className,
+  imageUrl,
+  title,
+  details,
+  price,
+  payment
+}) => (
+  <Card className={className}>
+    <VehicleImage alt="vehicle-photo" src={imageUrl} />
+    <VehicleInfo>
+      <Title>{title}</Title>
+      <Details>{details.join(' | ')}</Details>
+      <PriceLine>
+        <div>{price}</div>
+        <div>{payment}</div>
+      </PriceLine>
+    </VehicleInfo>
+  </Card>
 )
 
 export default VehicleCard
--- src/components/VehicleCard/styles.js
+++ src/components/VehicleCard/styles.js
@@ -6,19 +6,15 @@ export const VehicleImage = styled.img`
   width: 100%;
 `
 
-export const Title = styled(Typography)`
-  padding-left: ${({ theme }) => theme.mui.spacing(1)}px;
-  padding-right: ${({ theme }) => theme.mui.spacing(1)}px;
+export const VehicleInfo = styled.div`
+  padding: ${({ theme }) => theme.mui.spacing(1.5)}px;
 `
 
-export const Details = styled(Typography)`
-padding-left: ${({ theme }) => theme.mui.spacing(1)}px;
-padding-right: ${({ theme }) => theme.mui.spacing(1)}px;
-`
+export const Title = styled(Typography)``
+
+export const Details = styled(Typography)``
 
 export const PriceLine = styled(Typography)`
-  padding-left: ${({ theme }) => theme.mui.spacing(1)}px;
-  padding-right: ${({ theme }) => theme.mui.spacing(1)}px;
   display: flex;
   justify-content: space-between;
-` 
+`
