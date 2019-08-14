6de76b203285574de2430eb6e985d9e74daed148
Ryan O'Shaughnessy <ryano@purecars.com>
2019-08-14 14:24:22 -0400

    PR feedback
    Change component name.
    Pass in only image URLs as an array.
    A little cleanup.

--- src/components/ImageGrid/index.js
+++ src/components/ImageGrid/index.js
@@ -1,22 +1,21 @@
 import React from 'react'
 import styled from '@emotion/styled/macro'
 
-const Grid = styled.div`
+const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-context: space-between;
-  margin: 10px;
 `
 
-const Icon = styled.img`
+const Image = styled.img`
   width: 100px;
   height: 100px;
 `
 
-export const IconGrid = ({ icons }) => {
+export const ImageGrid = ({ imageUrls }) => {
   return (
-    <Grid>
-      {icons.map(icon => <Icon src={icon.imageUrl}/>)}
-    </Grid>
+    <Wrapper>
+      {imageUrls.map(imageUrl => <Image src={imageUrl}/>)}
+    </Wrapper>
   )
 }
