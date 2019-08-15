f6a54515136bfab439cbedc8ff04d128bd3f8841
Ryan O'Shaughnessy <ryano@purecars.com>
2019-08-15 02:20:43 -0400

    Add VDP
    Initial VDP implementation

--- src/loadables/VehicleDetails/index.js
+++ src/loadables/VehicleDetails/index.js
@@ -1,5 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import VehicleImagesBanner from '../../components/VehicleImagesBanner'
+import VehicleSummary from '../../components/VehicleSummary'
+import { ImageGrid } from '../../components/ImageGrid'
 
-export default function VehicleDetailsLoadable({ id }) {
-  return <span>vehicle id: {id}</span>
+async function getVehicleDetails(vehicleId) {
+  const response = await fetch(
+    `https://apix.purecars.com/joyride/public/vehicles/${vehicleId}`
+  )
+
+  const responseJson = await response.json()
+
+  console.log(responseJson)
+
+  return responseJson
+
+  // return responseJson.map(result => ({
+  //   id: result.id,
+  //   imageUrl: result.photo.url.thumb,
+  //   title: result.title,
+  //   details: [result.subtitle],
+  //   price: formatString('Currency')(result.price.cash),
+  //   payment: `${formatString('Currency')(result.price.monthly)}/month`
+  // }))
+}
+
+export default function VehicleDetailsLoadable({ id: vehicleId }) {
+  const [vehicleDetails, setVehicleDetails] = useState()
+  useEffect(() => {
+    async function fetchData() {
+      const results = await getVehicleDetails(vehicleId)
+      console.log(results)
+      setVehicleDetails(results)
+    }
+    fetchData()
+  }, [vehicleId])
+
+  getVehicleDetails(vehicleId)
+  return vehicleDetails ? (
+    <>
+      <VehicleImagesBanner vehicle={vehicleDetails}></VehicleImagesBanner>
+      <VehicleSummary vehicle={vehicleDetails}></VehicleSummary>
+      <ImageGrid
+        imageUrls={vehicleDetails.features
+          .filter(feature => feature.imageUrl)
+          .map(feature => feature.imageUrl)}
+      ></ImageGrid>
+    </>
+  ) : (
+    'loading...'
+  )
 }
