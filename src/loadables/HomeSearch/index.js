44048bab56f0d89dec95885c4b8fbe811f8a3658
Jordan Carroll <jordanc@purecars.com>
2019-08-14 21:30:00 -0400

    remeoving redirect

--- src/loadables/HomeSearch/index.js
+++ src/loadables/HomeSearch/index.js
@@ -9,33 +9,27 @@ function parseQuery(query) {
   return queryString.stringify({ year, make, model })
 }
 
-function SearchForm() {
+function SearchForm({ onSubmit }) {
   const [query, setQuery] = useState('')
-  const [search, setSearch] = useState(null)
 
   function handleSubmit(e) {
     e.preventDefault()
-    setSearch(parseQuery(query))
+    onSubmit(parseQuery(query))
   }
 
-  return search ? (
-    <Redirect
-      to={{
-        pathname: '/search',
-        search: `?${search}`
-      }}
-    />
-  ) : (
+  return (
     <form onSubmit={handleSubmit}>
       <TextField value={query} onChange={e => setQuery(e.target.value)} />
     </form>
   )
 }
 
-export default function HomeSearchLoadable() {
+export default function HomeSearchLoadable({ history }) {
   return (
     <LandingBanner
-      form={SearchForm}
+      form={() => (
+        <SearchForm onSubmit={search => history.push(`/search?${search}`)} />
+      )}
       title="Discover, drive, and buy with Joyride."
       image="https://firebasestorage.googleapis.com/v0/b/joyride-hacked.appspot.com/o/images%2Fvehicle.png?alt=media&token=319e2953-b093-4cd1-b2cb-2b1ee703ad04"
     />
